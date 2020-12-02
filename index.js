90671b518cb9c8ad58b0b527a67c9fdc2627e1c7
eunice <ewycb2020@gmail.com>
2020-12-02 21:37:35 +0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-console.log('hello~');
+// console.log('hello~');
 
-const os = require('os');
-console.log(os.hostname());
+// const { request } = require('http');
+// const os = require('os');
+// console.log(os.hostname());
 
-const reqest = require('request');
+// const request = require('request')
+// request({ url: 'https://tw.movies.yahoo.com/movie_thisweek.html',
+// json: true
+// }, (error, response, body) => {
+// console.log(body)  
+// //console.log(JSON.stringify(body, null, 2))
+// })
 
+const greeting = require('./src/greeting');
+
+greeting.hello();
